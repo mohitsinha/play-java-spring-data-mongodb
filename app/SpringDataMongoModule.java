87cf96d685e4c776e428e7636a7542f0ed8574d8
Mohit Sinha <mohitsinha251@gmail.com>
2017-04-08 20:27:29 +0530

    Made mongo url, port and database configurable.

--- app/SpringDataMongoModule.java
+++ app/SpringDataMongoModule.java
@@ -1,9 +1,12 @@
 import com.mongodb.MongoClient;
 import guice.module.SpringModule;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
@@ -30,12 +33,27 @@ public class SpringDataMongoModule extends SpringModule{
      */
 
     @Configuration
+    @PropertySource("classpath:config.properties")
     @EnableMongoRepositories("repositories")
     public static class SpringDataMongoConfiguration {
 
+        @Value("${mongodb.url}")
+        private String mongodbUrl;
+
+        @Value("${mongodb.port}")
+        private Integer mongodbPort;
+
+        @Value("${mongodb.database}")
+        private String mongodbDatabase;
+
+        @Bean
+        public static PropertySourcesPlaceholderConfigurer propertyConfigIn() {
+            return new PropertySourcesPlaceholderConfigurer();
+        }
+
         @Bean
         public MongoDbFactory mongoDbFactory() throws UnknownHostException {
-            return new SimpleMongoDbFactory(new MongoClient(),"play");
+            return new SimpleMongoDbFactory(new MongoClient(mongodbUrl, mongodbPort), mongodbDatabase);
         }
 
         @Bean
